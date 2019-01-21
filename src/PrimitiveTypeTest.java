aff7997aa6d2b99fd25570548eb695e4071bf531
LaoBing_Fung <FLB_B401@163.com>
2019-01-21 21:17:16 +0800

    print max and min value of 8 primitive type of java

---
+++ src/PrimitiveTypeTest.java
@@ -0,0 +1,22 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+public class PrimitiveTypeTest {
+    public static void main(String[] args) {
+        System.out.println("byte型最大值" + Byte.MAX_VALUE);
+        System.out.println("byte型最小值" + Byte.MIN_VALUE);
+        System.out.println("short型最大值" + Short.MAX_VALUE);
+        System.out.println("short型最小值" + Short.MIN_VALUE);
+        System.out.println("int型最大值" + Integer.MAX_VALUE);
+        System.out.println("int型最小值" + Integer.MIN_VALUE);
+        System.out.println("long型最大值" + Long.MAX_VALUE);
+        System.out.println("long型最小值" + Long.MIN_VALUE);
+        System.out.println("float型最大值" + Float.MAX_VALUE);
+        System.out.println("float型最小值" + Float.MIN_VALUE);
+        System.out.println("double型最大值" + Double.MAX_VALUE);
+        System.out.println("double型最小值" + Double.MIN_VALUE);
+        System.out.println("char型最大值" + (int)Character.MAX_VALUE);
+        System.out.println("char型最小值" + (int)Character.MIN_VALUE);
+        System.out.println("boolean型最大值" + Boolean.TRUE);
+        System.out.println("boolean型最小值" + Boolean.FALSE);
+    }
+}
